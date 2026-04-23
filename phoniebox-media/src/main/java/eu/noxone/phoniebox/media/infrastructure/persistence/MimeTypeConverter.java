c2020182705d66ba941dbdca121595d2f8ae64e3
Olaf Neumann <git@olafneumann.org>
2026-04-23 21:06:48 +0200

    Switch to reflective converter

--- phoniebox-media/src/main/java/eu/noxone/phoniebox/media/infrastructure/persistence/MimeTypeConverter.java
+++ phoniebox-media/src/main/java/eu/noxone/phoniebox/media/infrastructure/persistence/MimeTypeConverter.java
@@ -1,11 +1,9 @@
 package eu.noxone.phoniebox.media.infrastructure.persistence;
 
 import eu.noxone.phoniebox.media.domain.model.MimeType;
-import eu.noxone.phoniebox.shared.persistence.DomainAttributeConverter;
+import eu.noxone.phoniebox.shared.persistence.ReflectiveDomainAttributeConverter;
 import jakarta.persistence.Converter;
 
 /** Converts {@link MimeType} ↔ {@link String} for JPA persistence. */
 @Converter(autoApply = true)
-public class MimeTypeConverter extends DomainAttributeConverter<MimeType, String> {
-    public MimeTypeConverter() { super(MimeType::of, MimeType::getValue); }
-}
+public class MimeTypeConverter extends ReflectiveDomainAttributeConverter<MimeType, String> {}
--- phoniebox-media/src/main/java/eu/noxone/phoniebox/media/infrastructure/persistence/OriginalFileNameConverter.java
+++ phoniebox-media/src/main/java/eu/noxone/phoniebox/media/infrastructure/persistence/OriginalFileNameConverter.java
@@ -1,11 +1,9 @@
 package eu.noxone.phoniebox.media.infrastructure.persistence;
 
 import eu.noxone.phoniebox.media.domain.model.OriginalFileName;
-import eu.noxone.phoniebox.shared.persistence.DomainAttributeConverter;
+import eu.noxone.phoniebox.shared.persistence.ReflectiveDomainAttributeConverter;
 import jakarta.persistence.Converter;
 
 /** Converts {@link OriginalFileName} ↔ {@link String} for JPA persistence. */
 @Converter(autoApply = true)
-public class OriginalFileNameConverter extends DomainAttributeConverter<OriginalFileName, String> {
-    public OriginalFileNameConverter() { super(OriginalFileName::of, OriginalFileName::getValue); }
-}
+public class OriginalFileNameConverter extends ReflectiveDomainAttributeConverter<OriginalFileName, String> {}
